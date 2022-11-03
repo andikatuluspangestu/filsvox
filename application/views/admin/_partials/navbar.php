9844d157bdb1a44dea4ab9b6006769f719b3c4df
andikatuluspangestu <andikatulusp@gmail.com>
2022-11-03 21:30:35 +0700

    Improve Navbar Background Color
    Signed-off-by: andikatuluspangestu <andikatulusp@gmail.com>

--- application/views/admin/_partials/navbar.php
+++ application/views/admin/_partials/navbar.php
@@ -1,5 +1,5 @@
-<nav class="bg-white border-b border-gray-200 fixed z-30 w-full">
-  <div class="px-3 py-3 lg:px-5 lg:pl-3">
+<nav class="border-gray-200 fixed z-30 w-full">
+  <div class="px-3 py-3 lg:px-5 lg:pl-3 bg-gray-800">
     <div class="flex items-center justify-between">
       <div class="flex items-center justify-start">
         <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar" class="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded">
@@ -12,7 +12,7 @@
         </button>
         <a href="https://demo.themesberg.com/windster/" class="text-xl font-bold flex items-center lg:ml-2.5">
           <img src="https://cdn-icons-png.flaticon.com/512/3163/3163508.png" class="h-6 mr-2" alt="Windster Logo" />
-          <span class="self-center whitespace-nowrap">FILSVOX</span>
+          <span class="self-center text-white whitespace-nowrap">FILSVOX</span>
         </a>
 
       </div>
