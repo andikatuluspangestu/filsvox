6bc89bf2795501654575bfa2217519cabf4b07c1
andikatuluspangestu <andikatulusp@gmail.com>
2022-11-03 21:53:36 +0700

    Added Category Menu
    Signed-off-by: andikatuluspangestu <andikatulusp@gmail.com>

--- application/views/admin/_partials/side_nav.php
+++ application/views/admin/_partials/side_nav.php
@@ -10,7 +10,7 @@
                 <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
                 <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
               </svg>
-              <span class="ml-3">Dashboard</span>
+              <span class="ml-3 hover:text-gray-500">Dashboard</span>
             </a>
           </li>
           <li>
@@ -19,7 +19,16 @@
                 <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
                 </path>
               </svg>
-              <span class="ml-3 flex-1 whitespace-nowrap">Artikel</span>
+              <span class="ml-3 flex-1 whitespace-nowrap hover:text-gray-500">Article</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="text-gray-900 font-normal rounded-lg hover:bg-gray-100 hover:text-gray-900 flex items-center p-2 group">
+              <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" clip-rule="evenodd" />
+              </svg>
+              <span class="ml-3 flex-1 whitespace-nowrap hover:text-gray-500">Category</span>
             </a>
           </li>
 
@@ -28,7 +37,7 @@
               <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
               </svg>
-              <span class="ml-3 flex-1 whitespace-nowrap">Users</span>
+              <span class="ml-3 flex-1 whitespace-nowrap hover:text-gray-500">Users</span>
             </a>
           </li>
         </ul>
