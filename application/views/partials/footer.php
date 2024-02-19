2a4af1a78ea495d3059360ed423023a15d9b9772
andikatuluspangestu <andikatulusp@gmail.com>
2024-02-19 16:43:03 +0700

    Update footer.php with current year and add JavaScript libraries

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,7 +1,10 @@
 <footer class="bg-slate-900 text-white pt-12 pb-8 px-4">
   <div class="pt-4 mt-4 pt-6 text-gray-600 border-t border-gray-800 flex flex-col md:flex-row justify-center items-center">
-    <div>© 2022 FILSVOX Entertainment Co.LTD.</div>
+    <div>©<?= date('Y'); ?> FILSVOX Entertainment Co.LTD.</div>
     <div>&nbsp;All rights reserved.</div>
   </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.13.5/cdn.js" integrity="sha512-8IPRU0MPrge2KfSxkAtO8pIkaMzThW/MBSvPqcyVisSymLWC986buo27pKAt5mWXmt58dT6jIsw7h8NNugtRwg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </footer>
